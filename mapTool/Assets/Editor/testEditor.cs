6253a3ea4a635513c71d1b7cceffc91cd77a5162
DanDotPng <mynameisntimportantstopasking@gmail.com>
2022-12-06 18:12:27 +0000

    disc broken, line implemented

--- mapTool/Assets/Editor/testEditor.cs
+++ mapTool/Assets/Editor/testEditor.cs
@@ -1,26 +1,46 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
 public class testEditor : EditorWindow
-{
-    public float radius = 1f;
-    public float amount = 2f; 
-
-
-    //This creates the menu at the top. We use the name "tools" since thats a common library name and prevents clutter.
+{//This creates the menu at the top. We use the name "tools" since thats a common library name and prevents clutter.
     [MenuItem("Tools/Test Library")]
+    public static void ShowWindow() => GetWindow<testEditor>();
 
-    public static void ShowWindow()
+    public float radius = 2f;
+    public float amount = 2;
+
+    //disables GUI when not using the scene view. so like if you click out or something.
+    void OnEnable() => SceneView.duringSceneGui += DuringSceneGUI;
+    void OnDisable() => SceneView.duringSceneGui -= DuringSceneGUI;
+ 
+    
+    SerializedObject so;
+    SerializedProperty propRadius;
+    SerializedProperty propSpawnCount;
+    //private void OnGUI()
+  
+    //while the scene is active
+    void DuringSceneGUI(SceneView sceneView)
     {
-        EditorWindow.GetWindow(typeof(testEditor));
-    }
+        Transform cTransform = sceneView.camera.transform;
 
-    void OnGUI()
-    {
+        //raycast from the front of the camera
+        Ray ray = new Ray(cTransform.position, cTransform.forward);
+        // if hit
+        if(Physics.Raycast( ray, out RaycastHit hit))
+        {
+            //draw a little line and a circle based around the hit point
+            Handles.DrawAAPolyLine(6, hit.point, hit.point + hit.normal);
+            Handles.DrawWireDisc( hit.point, hit.normal, radius);
+
+        }
 
     }
-        
-     
+    
+
+
+
 }
